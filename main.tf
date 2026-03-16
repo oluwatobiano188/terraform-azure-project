2dadfb78dd38d346adac1b9afdff725691fa1e9f
oluwatobi <isholaoluwatobi10@gmail.com>
2026-03-16 12:18:47 +0000

    initial terraform project

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.project}-rg"
+  location = var.location
+}
+
+locals {
+  project = "devopsdemo"
+}
